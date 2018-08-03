5d5b57bacc1d55802c0ca455763da5f49b8b6264
haowei1029 <zhuhaowei1994@gmail.com>
2018-08-03 22:28:08 +1200

    making input box

--- haowei-app/src/components/FirstComponent.tsx
+++ haowei-app/src/components/FirstComponent.tsx
@@ -6,7 +6,10 @@ export default class FirstComponent extends React.Component<{}> {
                 return (
                         <div className="centreText">
                                 {/* React components must have a wrapper node/element */}
-                                <h1>(▀̿Ĺ̯▀̿ ̿)</h1>
+
+                                <h5>Enter a country name in the box</h5>
+                                Country: <input type="text" name=""/>
+                                <input type="submit" value="Go"/>
                         </div>
                 );
         }
