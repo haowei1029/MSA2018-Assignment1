7c1f1f25575d5a0ea5fd9422257ec60c341de2d1
haowei1029 <zhuhaowei1994@gmail.com>
2018-08-03 17:42:20 +1200

    Using meterial from Phase 1 training

--- haowei-app/src/components/Header.tsx
+++ haowei-app/src/components/Header.tsx
@@ -1,24 +1,25 @@
+import {AppBar, IconButton, Toolbar, Typography} from '@material-ui/core/';
+import MenuIcon from '@material-ui/icons/Menu';
 import * as React from 'react';
-import { Nav, Navbar, NavItem } from 'react-bootstrap';
-import { IndexLinkContainer } from "react-router-bootstrap";
+// import { Nav, Navbar, NavItem } from 'react-bootstrap';
+// import { IndexLinkContainer } from "react-router-bootstrap";
 import { Link } from 'react-router-dom';
+// import Menu from '@material-ui/core/Menu'
+
 
 export const Header: React.StatelessComponent<{}> = () => {
     return (
-        <Navbar>
-            <Navbar.Header>
-                <Navbar.Brand>
-                    <Link to="/">dankNotDank</Link>
-                </Navbar.Brand>
-            </Navbar.Header>
-            <Nav>
-                <IndexLinkContainer to="/FirstComponent">
-                    <NavItem>Time</NavItem>
-                </IndexLinkContainer>
-                <IndexLinkContainer to="/SecondComponent">
-                    <NavItem>Weather</NavItem>
-                </IndexLinkContainer>
-            </Nav>
-        </Navbar>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton  aria-label="Menu" color="inherit">
+                        <MenuIcon aria-haspopup="true"/>
+                    </IconButton>
+                    <Typography variant="display2" color="inherit">
+                        <Link style={{color: "white"}} to="/">dankNotDank</Link>
+                        <Link to="/FirstComponent"> Page 1 </Link>
+                        <Link to="/SecondComponent"> Page 2 </Link>
+                    </Typography>
+                </Toolbar>
+            </AppBar>
     );
 }
